f5484064177bb51f415ca310208790b621c1684d
Jimmy Tran <jimmytran1620@gmail.com>
2021-09-15 16:37:46 -0400

    deploy again - fixed port

--- start.js
+++ start.js
@@ -1,3 +1,3 @@
 const app = require('./server')
-
-app.listen(4001, () => console.log(`Connection to PORT ${ 4001 }`));
+const port = process.env.PORT || 4001;
+app.listen(port, () => console.log(`Connection to PORT ${ port }`));
